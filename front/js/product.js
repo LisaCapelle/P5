a59562c36aaf8f6afc73ec8e01287e414e7859ec
Lisa Capelle <capelle.box@gmail.com>
2022-08-12 22:49:22 +0200

    Finalized index page, started product page. Display of product on product page done.

--- front/js/product.js
+++ front/js/product.js
@@ -1,72 +1,68 @@
-function getProductList() {
-//fetch ou get l'API
-// sur URL http://localhost:3000/api/products
-// console.log(response)
-// const productList = response;
-//faire une boucle tant que produit <= au productlist.length
-//for (let i =0; i < totalItems; i++{}
-//
+// elements to add on product page
+let imgElement = document.querySelector(".item__img");
+let titleElement = document.querySelector("#title");
+let priceElement = document.querySelector("#price");
+let descriptionElement = document.querySelector("#description");
+let colorsElement = document.getElementById("colors");
 
-// class ProductItem {
-// constructor(name, ID, description, price, imageURL, altTxt, colors)
-//{this.name = name;
-//this.ID = ID;
-//this....
-//}
-//}
-// const selectedItem = onclick();
-//showName(){
-//selectedItem.name
-//console.log("product name" + this.name);
-//}
-//selectedItem.showName();
-//
-//
+// get product id from URL
+const pageString = window.location.search;
+const urlParams = new URLSearchParams(pageString);
+const id = urlParams.get("id");
 
-// const Items = [{name:"A",ticket:"1"}, {name:"2"...}] 
-//for (let Item of ItemsList) {console.log(Item)}
-
-
-//const generateItem = (Show) =>{
-//const nameProduct = show.name;
-//const IDProduct = show.ID;
-//return{nameProduct, IDProduct,};
-//const firstItem = generateItem [first];
-//const showItem =[first, second, third...];
-//
-//
-//
-//}
-//
-//
-//
-//
-//
-//
-
-
-
-
-
-// let selectedItem = true;
-// if(selecterItem) {
-    //console.log (response);
-    //}
-
-//else{
-//console.log ("non-disponible");
-//}
-
-// let itemsAvailable = 20;
-// let itemsToAddToBasket = 2;
-// let itemsInBasket = 0;
-// while (itemsList >0 && itemsToAddToBasket > 0){itemsInBasket++; ItemsToAddToBasket--;itemsAvailable--;} console.log(itemsInBasket);
-//
-//
-// if (dataExists && dataIsValid) {}
-// else {}
-// try{}
-// catch(error){}
-
-
+// get the clicked product via API
+function getProduct() {
+    fetch(`http://localhost:3000/api/products/${id}`)
+        .then(response => { if (response.ok) { return response.json(); } })
+        .then(product => {displayProduct(product)})
+        .catch(function (error) {alert('une erreur est survenue')});
 }
+getProduct();
+
+// display colors of the clicked product
+function getColorOptions(product) {
+    console.log(product);
+    for (let x = 0; x < product.colors.length; x++){
+        colorsElement.innerHTML += `<option value="${product.colors[x]}"> 
+        ${product.colors[x]}</option>`
+    }
+}
+
+// display all elements of the clicked product via API
+function displayProduct(product){
+    imgElement.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}"></img>`;
+    titleElement.innerHTML = `<h1 id="title">${product.name}</h1>`;
+    priceElement.innerHTML = `<span id="price">${product.price}</span>`;
+    descriptionElement.innerHTML = `<p id="description">${product.description}</p>`;
+    getColorOptions(product);
+}
+
+// add to local storage
+let addToCartBtn = document.querySelector("#addToCart");
+let color = document.querySelector("#colors")
+let quantity = document.querySelector("#quantity")
+
+// add selected product options to local storage on click
+addToCartBtn.addEventListener('click', e => {
+    if (color.value === "") {
+        alert("Veuillez sélectionner une couleur")
+    } else if (quantity.value > 0 && quantity.value < 100) {
+            const productOptions = {
+            id: productId,
+            color: color.value,
+            quantity: quantity.value,
+//         }
+//         addToCart(productOptions)
+//     } else if (quantity.value > 100){alert("Maximum 100 produits par commande")}
+// })
+
+// // get seleted product
+// const getSelectedProduct = id => {
+//     const selectedColor = colorsElement.options[colorsElement.selectedIndex].value;
+//     const selection = {
+//         id:id,
+//         quantity: + quantityElement.value,
+//         color:selectedColor
+//     };
+//     return selection;
+// }
--- front/js/script.js
+++ front/js/script.js
@@ -2,7 +2,8 @@
 function getProductList() {
     fetch("http://localhost:3000/api/products")
         .then(response => {
-            if (response.ok) { return response.json(); }
+            if (response.ok) { 
+                return response.json(); }
         })
         .then(function (productList) {
 
@@ -10,11 +11,17 @@ function getProductList() {
             const numberOfProducts = productList.length;
             for (let x = 0; x < numberOfProducts; x++) {
                 console.log(productList[x]);
-             const items = document.getElementById('items');
- items.innerHTML += `<a href="./product.html?id=${productList[x]._id}"><article><img src="${productList[x].imageUrl}" alt="${productList[x].altTxt}"><h3 class="productName">${productList[x].name}</h3><p class="productDescription">${productList[x].description}Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p></article></a>`
-             }
+                const items = document.getElementById('items');
+                items.innerHTML += `<a href="./product.html?id=${productList[x]._id}">
+                <article>
+                <img src="${productList[x].imageUrl}" alt="${productList[x].altTxt}">
+                <h3 class="productName">${productList[x].name}</h3>
+                <p class="productDescription">${productList[x].description}</p>
+                </article>
+                </a>`
+            }
         })
-         .catch(function (err) {//an error has occured
-         });
- }
+        .catch((error) => {alert("Une erreur est survenue")
+        });
+}
 getProductList();
