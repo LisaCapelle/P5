d82bfe9d42ea3169ac53550eae69178ce74fc530
Lisa Capelle <capelle.box@gmail.com>
2022-08-26 17:22:31 +0200

    Réécriture PRODUIT

--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,5 @@
-// éléments à ajouter à la page produits  - OK 
+// A. Affichage du produit sélectionné
+// éléments à ajouter à la page produits - OK 
 let imgElement = document.querySelector(".item__img");
 let titleElement = document.querySelector("#title");
 let priceElement = document.querySelector("#price");
@@ -25,11 +26,9 @@ getProduct();
 // afficher les couleurs du produit cliqué - OK
 function getColorOptions(product) {
     console.table(product);
-    // const ColorArray = product.colors;
-    // for (color of colorArray){colorsElement.innerHTML += '<option value ="${color}"> ${color}</option>'}
-    for (let x = 0; x < product.colors.length; x++) {
-        colorsElement.innerHTML += `<option value="${product.colors[x]}"> 
-        ${product.colors[x]}</option>`
+    for (let i = 0; i < product.colors.length; i++) {
+        colorsElement.innerHTML += `<option value="${product.colors[i]}"> 
+        ${product.colors[i]}</option>`
     }
 }
 
@@ -43,158 +42,76 @@ function displayProduct(product) {
     getColorOptions(product);
 }
 
+//B. créer la fonction d'ajouter le produit au Local Storage
+function addProduct() {
 
-// sauvegarder les données du produit dans le Web Storage
+// 1. Sélectionner le choix de couleur et le mettre dans une variable
+    const selectedColor = colorsElement.options[colorsElement.selectedIndex].value;
 
-// 1. Sélectionner le bouton d'ajout au panier et y rajouter Event Listener
+// 2. Sélectionner le choix de quantité et le mettre dans une variable
+    const selectedQuantity = quantityElement.value;
+
+// 3. créer un objet produit avec clés + variables ci-dessus
+const selectedProduct = {
+    selectedProductId: id,
+    selectedProductColor: selectedColor,
+    selectedProductQuantity: selectedQuantity
+} 
+// 4. vérifier que les données entrées sont correctes et si oui les sauvegarder
+if (selectedColor === ""){
+    alert("veuillez choisir une couleur")
+}else if(selectedQuantity < 0 || selectedQuantity > 100){
+    alert("veuillez saisir qté entre 1 et 100")
+}else{
+    let productSavedInLS;
+    localStorage.getItem("Cart");
+    console.log(localStorage.getItem("Cart"));
+
+    if (localStorage.getItem("Cart") == null){
+        productSavedInLS = [];
+    }
+    else {
+        productSavedInLS = JSON.parse(localStorage.getItem("Cart"))
+    }
+
+    let findProduct = productSavedInLS.find((p) => {
+        console.log(productSavedInLS);
+        return p.selectedProductId === selectedProduct.id && p.selectedProductColor === selectedProduct.selectedProductColor
+    })
+    if(!findProduct){
+        console.log("pusher le produit sélectionné dans le tableau vide");
+        productSavedInLS.push(selectedProduct);
+        console.log("ajouter le tableau mis à jour dans le LS")
+        localStorage.setItem("Cart", JSON.stringify(productSavedInLS))
+    }
+    else{
+        console.log("changer qté");
+        let totalQuantity = Number(findProduct.selectedProductQuantity) + Number(selectedProduct.selectedProductQuantity);
+        if (totalQuantity > 100){
+            alert("attention maximum 100")
+        }
+        else{
+            selectedProduct.selectedQuantity = totalQuantity;
+            productSavedInLS.push(selectedProduct);
+            console.log("ajouter le tableau mis à jour dans le LS")
+            localStorage.setItem("Cart", JSON.stringify(productSavedInLS))
+        }
+    }
+};
+
+         
+    //si produit déjà dans panier avec le meme couleur
+    // dans ce cas modifier la quantite et verifier qu'elle ne
+    // depasse pas 100
+    // si produit pas dans le panier l'ajouter
+
+}
+
+// C. Sélectionner le bouton d'ajout au panier et y rajouter Event Listener pour exécuter la fonction au clic
 const addToCartBtn = document.querySelector("#addToCart");
 addToCartBtn.addEventListener("click", (e) => {
     e.preventDefault();
-// 2. Sélectionner le choix de couleur et le mettre dans une variable
-    selectedColor = colorsElement.value;
-// 3. Sélectionner le choix de quantité et le mettre dans une variable
-    selectedQuantity = quantityElement.value;
-
-//*ESSAI
-
-// 4. ajouter le produit dans le LS si les conditions sont réunies
-// 4.1. déclarer la variable pour stocker temporairement et mettre à jour la table des produits dans Web Storage:
-//     var tabProducts = [];
-
-// 4.2. créer une fonction de sauvegarde de produit dans le Web Storage: 
-//     function saveBasket(basket) {localStorage.setItem("basket", JSON.stringify(basket))}
-//     function getBasket() {
-//         let basket = localStorage.getItem("basket");
-//         console.log("basket");
-//         if(basket == null){return[]}
-//         else{return JSON.parse(basket);}
-//     }    
-// 4.3. créer une fonction de rajout de produit dans le Web Storage si les conditions sont réunies 
-//function addBasket (product){
-//         let basket = getBasket();
-//         let foundProduct = basket.find(p => p.id == product.id)
-//         if(foundProduct != undefined){foundProduct.quantity + selectedQuantity}
-//         else {product.quantity = selectedQuantity;}
-//         tabProducts.push(product);
-//         saveBasket(basket);}
-//     })
-
-//  4.4.  // condition: vérifier si le produit est déjà dans le LS
-// // let productFound = productSavedInLS.find
-//     // ajouter produit dans le LS s'il n'y est pas 
-
-//     // s'il y est incrémenter la quantité dans LS avec celle entrée par l'utilisateur.
-
-
-    //*FIN ESSAI
-
-    // // get the data in selectedProductInfos variable provided user entered required data
-    // if (selectedColor !== "" && selectedQuantity > 0 && selectedQuantity <= 100) {
-    //     let productOptions = {
-    //         selectedProductId: id,
-    //         selectedProductColor: selectedColor,
-    //         selectedProductQuantity: selectedQuantity
-    //     }
-    
-    //     // create variable for message about LS updates
-    //     let messageUpdatesCart = false;
-
-    //     // function add selected product with its options id, color and quantity to LS 
-
-    //     const addProductToLS = () => {
-
-    //         // check if product and color are already in LS. If so, amend only quantity
-    //             let findProduct = productSavedInLS.find((p) => {
-    //                 console.log(productSavedInLS);
-    //                 return p.selectedProductId === productOptions.id && p.selectedProductColor === productOptions.selectedProductColor
-    //             });
-    //             if (findProduct) {
-    //                 const total = Number(findProduct.selectedProductQuantity) + Number(productOptions.selectedProductQuantity);
-    //                 console.log(total);
-
-    //                 if (total <= 100) {
-    //                     // switch message variable to false to display the corresponding message
-    //                     messageUpdatesCart = false;
-    //                     findProduct.selectedProductQuantity = Number(findProduct.selectedProductQuantity) + Number(productOptions.selectedProductQuantity);
-    //                     alert(`La quantité du produit ${titleElement.textContent} coloris ${selectedColor} a été mise à jour.`);
-    //                 }
-    //                 else {
-    //                     // switch message variable to false to display the corresponding message
-    //                     messageUpdatesCart = false;
-    //                     alert("Vous pouvez commander maximum 100 unités d'un même article d'une même couleur. Veuillez corriger la quantité souhaitée.");
-    //                 }
-    //             }
-
-    //             // if product and the color are not yet in LS: add product and its options id, color and quantity into LS
-    //             else {
-    //                 // switch message variable to true to display message
-    //                 messageUpdatesCart = true;
-    //                 // put options into variable "productSavedInLS"
-    //                 productSavedInLS.push(productOptions);
-    //             }
-
-    //             // transform into JSON and save data in LS with key "productsInCart"
-    //             localStorage.setItem("contentsInCart", JSON.stringify(productSavedInLS))
-    //         }
-
-    //         // create variable "productSavedInLS" to save keys and values and transform into JS object
-    //         let productSavedInLS = JSON.parse(localStorage.getItem("productsInCart"));
-
-    //         // if LS already has a key "productsInCart"
-    //         if (productSavedInLS) {
-    //             addProductToLS(); console.log(productSavedInLS);
-    //         }
-    //         // if LS has no key "productsInCart" it is empty
-    //         else {
-    //             productSavedInLS = [];
-    //             addProductToLS(); console.log(productSavedInLS);
-    //             // switch message variable to false to display the corresponding message
-    //             messageUpdatesCart = false;
-    //             alert(`Vous venez d'ajouter votre premier produit dans le panier.`);
-    //         }
-    //         // if the variable messageUpdatesCart is true the following message is displayed:
-    //         if (messageUpdatesCart) {
-    //             alert(`Vous venez de rajouter ${selectedQuantity} unités du produit ${titleElement.textContent} coloris ${selectedColor} au panier.`);
-    //         }
-    //     }
-    //     // if color not selected or quantity not between 1 and 100: display following message:
-    //     else {
-    //         alert(`La couleur n'est pas sélectionnée et/ou la quantité n'est pas comprise entre 1 et 100`);
-    //     }
-
-    })
-
-// ***CONSEILS
-//Pour faire un calcul numérique, on utilise parseInt() ou Number().
-//const entree1 = document.querySelector('myInput1').value;
-//const entree2 = document.querySelector('myInput2').value;
-//ex : var nb = parseInt(entree1 + entree2);
-//our cette histoire de tableau temporaire, il faut l'instancier en haut de ta page mais pas en tant que "constante", puisque le but est de pouvoir le modifier, et supprimer des éléments à l'avenir, donc ce sera en var.
-//Tu peux le déclarer comme ça :  var tabProduits = [];
-//Ensuite, très simple d'y ajouter des entrées à l'aide de push(), de cette façon => tabProduits.push('un truc en plus');
-//Utilise tabProduits.splice(i, 1) pour supprimer une entrée du tableau indiquée par sa position i de ce même tableau.
-
-
-// ***CATCH A LA FIN
-// .catch((error) => {
-// console.log("Erreur fetch product.js : l'id du produit est incorrect.", error);
-// alert(`Erreur: le produit sélectionné n'a pas été trouvé !`);
-// window.location.href = "index.html";
-// })
-// else{
-// console.log("L'id produit n'est pas indiqué dans l'url.");
-// alert(`Erreur: le produit sélectionné n'a pas été trouvé.`);
-// window.location.href = "index.html";
-// }
-
-
-
-
-
-
-
-
-
+    addProduct();})
 
 
 
--- front/js/script.js
+++ front/js/script.js
@@ -9,14 +9,14 @@ function getProductList() {
 
 // function to display the whole list of products on index.html page
             const numberOfProducts = productList.length;
-            for (let x = 0; x < numberOfProducts; x++) {
-                console.log(productList[x]);
+            for (let i = 0; i < numberOfProducts; i++) {
+                console.log(productList[i]);
                 const items = document.getElementById('items');
-                items.innerHTML += `<a href="./product.html?id=${productList[x]._id}">
+                items.innerHTML += `<a href="./product.html?id=${productList[i]._id}">
                 <article>
-                <img src="${productList[x].imageUrl}" alt="${productList[x].altTxt}">
-                <h3 class="productName">${productList[x].name}</h3>
-                <p class="productDescription">${productList[x].description}</p>
+                <img src="${productList[i].imageUrl}" alt="${productList[i].altTxt}">
+                <h3 class="productName">${productList[i].name}</h3>
+                <p class="productDescription">${productList[i].description}</p>
                 </article>
                 </a>`
             }
