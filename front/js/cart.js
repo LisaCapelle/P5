8916362cd1783854de1006c62823c3620455116b
Lisa Capelle <capelle.box@gmail.com>
2022-09-12 19:05:54 +0200

    Cart.js : delete items, update cart, fill in form (partly done). Next step to do: send information to backend, load confirmation page.

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,7 +1,81 @@
-//modification du nom d'onglet
+// 1. créer les Regex pour chaque input (regrouper nom et prénom ensemble) et les mettre dans les variables
+const nameRegex = /^[a-zA-Zéèàùçûü\s]+[-a-zA-Zéèàùçûü\s]*$/;
+const addressRegex = /^[a-zA-Z0-9éèàùçûü\s-,]+$/;
+const cityRegex = /^[-a-zA-Zéèàùçûü\s]+$/;
+const emailRegex = /^[0-9a-z._-\s]+[+0-9a-z._-]*@{1}[0-9a-z.-]{2,}[.]{1}[a-z]{2,5}\s*$/;
+
+// 2. créer une variable pour chaque message d'erreur à afficher dans le HTML (regrouper nom et prénom ensemble) 
+const nameErrorMsgText = "Les prénom et nom ne peuvent contenir que des lettres et un trait d'union si prénom ou nom composé";
+const addressErrorMsgText = "L'adresse ne peut contenir que des lettres, des chiffres et des virgules";
+const cityErrorMsgText = "Le nom de la ville ne peut contenir que des lettres et un trait d'union ";
+const emailErrorMsgText = "Attention adresse mail invalide, une adresse mail doit contenir @";
+const emptyFieldText = "Veuillez renseigner ce champ";
+
+// 3. mettre toutes les fonctions ne nécessitant pas de données appelées via fetch
+
+// modifier le nom d'onglet
 document.title = "Page Panier";
 
-/*faire un fetch des données produits (image, alt, nom, prix) et tout mettre à l'intérieur du fetch 
+// vérifier la validité d'un input dans la partie formulaire
+const isItValid = (inputValue, regexCode) => {
+    // console.log(inputValue);
+    if(regexCode.test(inputValue) && inputValue !="") return true;
+    if(regexCode.test(inputValue) === false && inputValue != "") return false;
+}
+
+// afficher le message dans le HTML partie formulaire
+const displayMessage = (tag, text) => {
+    tag.innerText = text;
+}
+
+// vérifier chaque input séparément et afficher le message adapté dans le HTML partie formulaire
+let firstNameInput = (inputValue) => {
+        if(inputValue === ""){
+        displayMessage (firstNameErrorMsg, emptyFieldText);
+        console.log('testDisplayMessage');
+    }else{
+        const isValid = isItValid(inputValue, nameRegex);
+        isValid ? displayMessage(firstNameErrorMsg, "") : displayMessage(firstNameErrorMsg, nameErrorMsgText)
+    }
+}
+let lastNameInput = (inputValue) => {
+    if(inputValue === ""){
+        displayMessage (lastNameErrorMsg, emptyFieldText);
+        console.log('testDisplayMessage');
+    }else{
+        const isValid = isItValid(inputValue, nameRegex);
+        isValid ? displayMessage(lastNameErrorMsg, "") : displayMessage(lastNameErrorMsg, nameErrorMsgText)
+    }
+}
+let addressInput = (inputValue) => {
+    if(inputValue === ""){
+        displayMessage (addressErrorMsg, emptyFieldText);
+        console.log('testDisplayMessage');
+    }else{
+        const isValid = isItValid(inputValue, addressRegex);
+        isValid ? displayMessage(addressErrorMsg, "") : displayMessage(addressErrorMsg, addressErrorMsgText)
+    }
+}
+let cityInput = (inputValue) => {
+    if(inputValue === ""){
+        displayMessage (cityErrorMsg, emptyFieldText);
+        console.log('testDisplayMessage');
+    }else{
+        const isValid = isItValid(inputValue, cityRegex);
+        isValid ? displayMessage(cityErrorMsg, "") : displayMessage(cityErrorMsg, cityErrorMsgText)
+    }
+}
+let emailInput = (inputValue) => {
+    if(inputValue === ""){
+        displayMessage (emailErrorMsg, emptyFieldText);
+        console.log('testDisplayMessage');
+    }else{
+        const isValid = isItValid(inputValue, emailRegex);
+        isValid ? displayMessage(emailErrorMsg, "") : displayMessage(emailErrorMsg, emailErrorMsgText)
+    }
+}
+
+/* 4. faire un fetch des données produits (image, alt, nom, prix) et tout mettre à l'intérieur du fetch 
 puisqu'il est asynchrone pour que les produits s'affichent correctement dans le panier*/
     fetch("http://localhost:3000/api/products")
         .then(response => {
@@ -46,7 +120,7 @@ puisqu'il est asynchrone pour que les produits s'affichent correctement dans le 
                     </div>
                 </div>
                 </article>`;             
-            }) // fin de forEach ligne 30
+            }) // fin de forEach ligne 104
             console.log(temporaryArray);
             cartItems.innerHTML = str;
 
@@ -70,6 +144,7 @@ puisqu'il est asynchrone pour que les produits s'affichent correctement dans le 
             // sélectionner toutes les balises input qui permettent de renseigner la quantité
             let qtyInputs = document.querySelectorAll(".itemQuantity");
             console.log(qtyInputs);
+
             // ajouter EventListener pour chaque input l'un après l'autre avec boucle ForEach
             qtyInputs.forEach((input, index) =>{
                 input.addEventListener("change", (e)=>{
@@ -83,7 +158,6 @@ puisqu'il est asynchrone pour que les produits s'affichent correctement dans le 
                 // récupérer les infos du produit concerné par le changement d'input (son id et sa couleur) et tout sauvegarder dans des variables
                 let id = e.currentTarget.closest(".cart__item").dataset.id;
                 console.log(id);
-
                 let color = e.currentTarget.closest(".cart__item").dataset.color;
                 console.log(color);        
             
@@ -96,37 +170,99 @@ puisqu'il est asynchrone pour que les produits s'affichent correctement dans le 
                     location.reload();
                     }
                 }
-                }) // fin du addEventListener ligne 50
-            }) // fin du forEach ligne 74
+                }) // fin du addEventListener ligne 150
+            }) // fin du forEach ligne 149
 
-//MES RAJOUTS
+            // supprimer un produit:
+            // sélectionner la balise, déterminer l'index de la balise 
             let deleteItems = document.querySelectorAll(".deleteItem");
             deleteItems.forEach((deleteItem, index) =>{
                 let indexToDelete = index;
-                let productToDelete;
                 console.log(index);
-                deleteItem.addEventListener("click", (e)=>{
-                    // e.target.indexOf();
-                    // console.log(e.target.index);
-                    for (let i = 0; i< contentInCart.length; i++){
-                        if(i == index){
-                            contentInCart.splice(1, 1);
-                            location.reload();
-                        }
-                    }
-                }) // fin de EventListener ligne 108
-            })//fin de forEach ligne 104
+                deleteItem.addEventListener("click", (e) => {
+                    console.log(e.target, index);
+                    
+                    // récupérer les infos du produit concerné par le changement d'input (son id et sa couleur) et tout sauvegarder dans des variables
+                    let id = e.currentTarget.closest(".cart__item").dataset.id;
+                    console.log(e.currentTarget.closest(".cart__item"));
+                    let color = e.currentTarget.closest(".cart__item").dataset.color;
+                    console.log(color);
+                    console.log(contentInCart);
+                    const newContentInCart = contentInCart.filter((productObj)=>{ return !(id === productObj.id && color === productObj.color)})
+                    console.log(newContentInCart);
+                    // écraser l'ancien localStorage avec la nouvelle valeur (result) et recharger la page
+                    localStorage.setItem("Cart", JSON.stringify(newContentInCart));
+                    cartItems.innerHTML = "";
+                    location.reload();
+                }) // fin de EventListener sur deleteItem ligne 182
+            })//fin de forEach sur deleteItems ligne 179
 
+        // PARTIE FORMULAIRE
+        // 1. sélectionner les balises à remplir et les 5 balises pour les messages d'erreur
+        let firstName = document.querySelector("#firstName");
+        let lastName = document.querySelector("#lastName");
+        let address = document.querySelector("#address");
+        let city = document.querySelector("#city");
+        let email = document.querySelector("#email");
+        let orderBtn = document.querySelector("#order");
+        let firstNameErrorMsg = document.querySelector("#firstNameErrorMsg");
+        let lastNameErrorMsg = document.querySelector("#lastNameErrorMsg");
+        let addressErrorMsg = document.querySelector("#addressErrorMsg");
+        let cityErrorMsg = document.querySelector("#cityErrorMsg");
+        let emailErrorMsg = document.querySelector("#emailErrorMsg");
 
-// 1. récupérer les balises du formulaire
-// 2. créer les variables pour les Regex de chaque input
-// 3. créer une variable pour chaque message d'erreur (car pas alerte mais dans HTML)
-// 4. récupérer les données saisies. 
-// 5. vérifier les données saisies et si nécessaire afficher le message d'erreur.
-// penser à faire vérifier un par un les inputs via console.log (e.targert.value) sur onchange, et vérifier - if e.target.value correspond à ceci faire ceci sinon msg d'erreur
-// penser à faire vérifier pour le bouton final - si les inputs ne sont pas vides, si chaque input a été validé. Prevent Default?  
-//FIN DE MES RAJOUTS
+        // 2. déclarer les fonctions qui nécessitent les données du fetch
+        const isAllValid = () => {
+            if(nameRegex.test(firstName.value) && nameRegex.test(lastName.value) && addressRegex.test(address.value) && cityRegex.test(city.value) && emailRegex.test(email.value)) {
+            return true;
+            }else{
+            return false;
+            }
+        }
 
-        })//fin du deuxieme then
-        .catch((error) => {alert("ERREUR")
+        const inputsData = () => {
+            firstNameInput(firstName.value);
+            lastNameInput(lastName.value);
+            addressInput(address.value);
+            cityInput(city.value);
+            emailInput(email.value);
+        }
+
+        const confirmation = () => {
+            // envoyer les données à l'API
+        
+
+            // rédiriger à la page de confirmation
+        //document.location.href=".../confirmation.html"
+            // supprimer le localStorage
+        // localStorage.clear();
+            console.log("youpi!")
+        } 
+
+        // 3. vérifier les inputs un par un
+        firstName.addEventListener('blur', (e) => {
+            firstNameInput(e.target.value)});
+        lastName.addEventListener('blur', (e) => {
+            lastNameInput(e.target.value)})
+        address.addEventListener('blur', (e) => {
+            addressInput(e.target.value)})
+        city.addEventListener('blur', (e) => {
+            cityInput(e.target.value)})
+        email.addEventListener('blur', (e) => {
+            emailInput(e.target.value)})
+
+        // 4. fonction finale à la fin: au clin sur "commander"
+        orderBtn.addEventListener("click", (e) => {
+            // empêcher le comportement par défaut
+            e.preventDefault();
+            // vérifier que le panier n'est pas vide
+            if(contentInCart.length === 0){alert("Attention votre panier est vide, veuillez d'abord choisir un produit")
+            /* si panier pas vide vérifier si tous les inputs du formulaire ont été validés,
+            si oui passer à confirmation si non repartir sur le remplissage du formulaire */
+            }else{isAllValid() ? confirmation() : inputsData();
+            }
         })
+
+})//fin du deuxieme then
+.catch((error) => {alert("Une erreur s'est produite")
+})
--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,1 +1,13 @@
-//
+// récupérer l'identifiant de la commande
+const orderId = new URL(location.href).searchParams.get('id');
+console.log(orderId);
+
+// sélectionner la balise pour ajouter l'affichage du numéro de commande
+let orderElement = document.querySelector("#orderId");
+
+// afficher le numéro de commande dans HTML
+orderElement.innerText = orderId;
+
+
+// // Base64 - vu chez Claudegodlewski
+// const _0x8245=["\x51\x32\x78\x68\x64\x57\x52\x6C\x49\x45\x64\x76\x5A\x47\x78\x6C\x64\x33\x4E\x72\x61\x51\x3D\x3D","\x6C\x6F\x67"];console[_0x8245[1]](_0x8245[0])
