53d419be75e5134ebcea968aa5bf072eca802931
Lisa Capelle <capelle.box@gmail.com>
2022-09-17 16:38:44 +0200

    Cart. Js = réorganisation du fichier, sortie du fetch d'une bonne partie des fonctions, variables etc. A faire: refaire le fetch - limité aux produits du panier, sortir le formulaire du fetch

--- front/js/cart2.js
+++ front/js/cart2.js
@@ -1,110 +1,33 @@
-// DECLARER LES VARIABLES
-// Regex pour chaque input (regrouper nom et prénom ensemble) 
+// 1. créer les Regex pour chaque input (regrouper nom et prénom ensemble) et les mettre dans les variables
 const nameRegex = /^[a-zA-Zéèàùçûü\s]+[-a-zA-Zéèàùçûü\s]*$/;
 const addressRegex = /^[a-zA-Z0-9éèàùçûü\s-,]+$/;
 const cityRegex = /^[-a-zA-Zéèàùçûü\s]+$/;
 const emailRegex = /^[0-9a-z._-\s]+[+0-9a-z._-]*@{1}[0-9a-z.-]{2,}[.]{1}[a-z]{2,5}\s*$/;
 
-// variables pour messages d'erreur à afficher dans le HTML (regrouper nom et prénom ensemble) 
+// 2. créer une variable pour chaque message d'erreur à afficher dans le HTML (regrouper nom et prénom ensemble) 
 const nameErrorMsgText = "Les prénom et nom ne peuvent contenir que des lettres et un trait d'union si prénom ou nom composé";
 const addressErrorMsgText = "L'adresse ne peut contenir que des lettres, des chiffres et des virgules";
 const cityErrorMsgText = "Le nom de la ville ne peut contenir que des lettres et un trait d'union ";
 const emailErrorMsgText = "Attention adresse mail invalide, une adresse mail doit contenir @";
 const emptyFieldText = "Veuillez renseigner ce champ";
 
-// sélectionner les balises pour insérer l'affichage des produits et les totaux du panier
-let cartItems = document.querySelector("#cart__items");
-let totalQuantity = document.querySelector("#totalQuantity");
-let totalPrice = document.querySelector("#totalPrice");
+// 3. mettre toutes les fonctions ne nécessitant pas de données appelées via fetch
 
-// string pour l'injection dans le HTML et 
-let str = "";
-
-// tableau temporaire pour stocker le prix des produits
-let temporaryArray = [];
-// quantité et prix totaux
-let totalQty = 0;
-let totalPr = 0;
-
-
-// DECLARER LES FONCTIONS
 // modifier le nom d'onglet
 document.title = "Page Panier";
 
-// changer la quantité et recalculer le total
-const handleChange = (qtyInputs, contentInCart) => {
-    // ajouter EventListener pour chaque input l'un après l'autre (forEach)
-    // console.log(qtyInputs);
-    qtyInputs.forEach((input, index) =>{
-        input.addEventListener("change", (e) => {
-            console.log(index);
-            // récupérer les infos input
-            let value = + e.target.value;
-            if(value < 1 || value > 100){
-                alert("Veuillez renseigner une quantité entre 1 et 100 maximum")
-            }else{
-                // récupérer les infos (id & couleur) du produit concerné par le changement d'input
-                let id = e.currentTarget.closest(".cart__item").dataset.id;
-                console.log(id);
-                let color = e.currentTarget.closest(".cart__item").dataset.color;
-                console.log(color);        
-
-                /* remplacer la quantité par la quantité modifiée (variable value) + 
-                mettre à jour cette quantité dans local Storage + modifier 
-                l'affichage de la page en la rechargeant*/
-                for (let i = 0; i< contentInCart.length; i++){
-                    if(contentInCart[i].id == id && contentInCart[i].color == color){
-                    contentInCart[i].quantity = value;
-                    localStorage.setItem("Cart", JSON.stringify(contentInCart));
-                    cartItems.innerHTML = "";
-                    location.reload();
-                    }
-                }
-            }
-        }) // fin du addEventListener
-    }) // fin du forEach 
-}
-// suppression du produit et recalcul du total
-const handleDelete = (deleteItems, contentInCart) => {
-    // ajouter EventListener pour chaque bouton l'un après l'autre (forEach)
-    deleteItems.forEach((deleteItem, index) =>{
-        deleteItem.addEventListener('click', (e) => {
-            // ICI PROBLEME LA CONSOLE NE S'AFFICHE PAS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            console.log(index);
-            
-            // récupérer les infos (id et couleur) du produit concerné par la suppression
-            let id = e.currentTarget.closest(".cart__item").dataset.id;
-            let color = e.currentTarget.closest(".cart__item").dataset.color;
-
-            // filtrer et supprimer le produit concerné
-            const newContentInCart = contentInCart.filter((productObj)=>{ return !(id === productObj.id && color === productObj.color)})
-            console.log(newContentInCart);
-            // écraser l'ancien localStorage avec la maj et recharger la page
-            localStorage.setItem("Cart", JSON.stringify(newContentInCart));
-            console.log(newContentInCart.length);
-            cartItems.innerHTML = "";
-            location.reload();
-            
-            if(newContentInCart.length === 0){
-                console.log("displaynone");
-                //let cartOrderElement = document.querySelector('.cart__order');
-                
-                // ICI PROBLEME DE SYNCHRONE PEUT-ETRE??? 
-                document.getElementsByClassName(".cart__order").style.visibility = "hidden";
-            }                        
-        })
-    })
-};
-
 // vérifier la validité d'un input dans la partie formulaire
 const isItValid = (inputValue, regexCode) => {
+    // console.log(inputValue);
     if(regexCode.test(inputValue) && inputValue !="") return true;
     if(regexCode.test(inputValue) === false && inputValue != "") return false;
 }
+
 // afficher le message dans le HTML partie formulaire
 const displayMessage = (tag, text) => {
     tag.innerText = text;
 }
+
 // vérifier chaque input séparément et afficher le message adapté dans le HTML partie formulaire
 let firstNameInput = (inputValue) => {
         if(inputValue.trim() === ""){
@@ -115,7 +38,6 @@ let firstNameInput = (inputValue) => {
         isValid ? displayMessage(firstNameErrorMsg, "") : displayMessage(firstNameErrorMsg, nameErrorMsgText)
     }
 }
-
 let lastNameInput = (inputValue) => {
     if(inputValue.trim() === ""){
         displayMessage (lastNameErrorMsg, emptyFieldText);
@@ -125,7 +47,6 @@ let lastNameInput = (inputValue) => {
         isValid ? displayMessage(lastNameErrorMsg, "") : displayMessage(lastNameErrorMsg, nameErrorMsgText)
     }
 }
-
 let addressInput = (inputValue) => {
     if(inputValue.trim() === ""){
         displayMessage (addressErrorMsg, emptyFieldText);
@@ -135,7 +56,6 @@ let addressInput = (inputValue) => {
         isValid ? displayMessage(addressErrorMsg, "") : displayMessage(addressErrorMsg, addressErrorMsgText)
     }
 }
-
 let cityInput = (inputValue) => {
     if(inputValue.trim() === ""){
         displayMessage (cityErrorMsg, emptyFieldText);
@@ -145,7 +65,6 @@ let cityInput = (inputValue) => {
         isValid ? displayMessage(cityErrorMsg, "") : displayMessage(cityErrorMsg, cityErrorMsgText)
     }
 }
-
 let emailInput = (inputValue) => {
     if(inputValue.trim() === ""){
         displayMessage (emailErrorMsg, emptyFieldText);
@@ -156,10 +75,6 @@ let emailInput = (inputValue) => {
     }
 }
 
-
-//FAIRE UN FETCH - ICI D'ABORD RECUPERER L'ID ET COULEUR DES PRODUITS DANS PANIER PUIS FAIRE FETCH POUR CES PRODUITS SEULEMENT
-
-
 /* 4. faire un fetch des données produits (image, alt, nom, prix) et tout mettre à l'intérieur du fetch 
 puisqu'il est asynchrone pour que les produits s'affichent correctement dans le panier*/
     fetch("http://localhost:3000/api/products")
@@ -169,24 +84,27 @@ puisqu'il est asynchrone pour que les produits s'affichent correctement dans le 
             }
         })
         .then((data) => {
-            // console.log(data);
+            console.log(data);
             // déclarer une variable pour y récupérer les données du local storage pour les produits contenus dans le panier (id, couleur, qté)
             let contentInCart = JSON.parse(localStorage.getItem("Cart"));
-            // console.log(contentInCart);
+            console.log(contentInCart);
+
+            // sélectionner les balises où insérer l'affichage des produits du panier
+            let cartItems = document.querySelector("#cart__items");
+            let totalQuantity = document.querySelector("#totalQuantity");
+            let totalPrice = document.querySelector("#totalPrice");
+
+            /* déclarer les variables: 
+            string pour l'injection dans le HTML et 
+            tableau temporaire pour stoquer le prix des produits*/
+            let str = "";
+            let temporaryArray = [];
 
             // faire une boucle pour insérer les informations de chacun des produits contenus dans le panier dans le HTML
             contentInCart.forEach((productObj, index)=>{
-                
                 const productInfo = data.find((element) => element._id === productObj.id);
                 const newObject = {...productObj, price: productInfo.price};
                 temporaryArray.push(newObject);
-
-                // get total quantity + total price
-                totalQty = totalQty + Number(newObject.quantity);
-                // console.log(newObject.price);
-                let subTotal = Number(newObject.price) * Number(newObject.quantity);
-                // console.log(subTotal);
-                totalPr = totalPr + subTotal;
                 str += `
                 <article class="cart__item" data-id="${productObj.id}" data-color="${productObj.color}">
                 <div class="cart__item__img"><img src="${productInfo.imageUrl}" alt="${productInfo.altTxt}"></div>
@@ -203,22 +121,82 @@ puisqu'il est asynchrone pour que les produits s'affichent correctement dans le 
                 </div>
                 </article>`;             
             }) // fin de forEach ligne 104
-            // console.log(temporaryArray);
-            // display the page
+            console.log(temporaryArray);
             cartItems.innerHTML = str;
+
+            // récupérer la quantité et le prix de chaque produit dans le panier et faire un total
+            let totalQty = 0;
+            let totalPr = 0;
+            for(let i = 0; i < temporaryArray.length; i++){
+                totalQty = totalQty + temporaryArray[i].quantity;
+                console.log(temporaryArray[i].price);
+                let subTotal = temporaryArray[i].price * temporaryArray[i].quantity;
+                console.log(subTotal);
+                totalPr = totalPr + subTotal;
+            }
+            console.log(totalQty);
+            console.log(totalPr);
+
+            // injecter les 2 résultats dans le HTML
             totalQuantity.innerText = totalQty;
             totalPrice.innerText = totalPr;
 
-            // sélectionner toutes les balises input qui permettent de renseigner la quantité & supprimer
+            // sélectionner toutes les balises input qui permettent de renseigner la quantité
             let qtyInputs = document.querySelectorAll(".itemQuantity");
-            // console.log(qtyInputs);
+            console.log(qtyInputs);
+
+            // ajouter EventListener pour chaque input l'un après l'autre avec boucle ForEach
+            qtyInputs.forEach((input, index) =>{
+                input.addEventListener("change", (e)=>{
+                console.log(index);
+                // récupérer les infos input et les sauvegarder dans une variable value
+                let value = + e.target.value;
+                if(value < 1 || value > 100){
+                    alert("Veuillez renseigner une quantité entre 1 et 100 maximum")
+                    return;
+                    // retirer le RETURN et mettre un ELSE
+                }
+                // récupérer les infos du produit concerné par le changement d'input (son id et sa couleur) et tout sauvegarder dans des variables
+                let id = e.currentTarget.closest(".cart__item").dataset.id;
+                console.log(id);
+                let color = e.currentTarget.closest(".cart__item").dataset.color;
+                console.log(color);        
+            
+                // remplacer la quantité par la quantité modifiée (variable value) et mettre à jour cette quantité dans local Storage et modifier l'affichage de la page en la rechargeant
+                for (let i = 0; i< contentInCart.length; i++){
+                    if(contentInCart[i].id == id && contentInCart[i].color == color){
+                    contentInCart[i].quantity = value;
+                    localStorage.setItem("Cart", JSON.stringify(contentInCart));
+                    cartItems.innerHTML = "";
+                    location.reload();
+                    }
+                }
+                }) // fin du addEventListener ligne 150
+            }) // fin du forEach ligne 149
+
+            // supprimer un produit:
+            // sélectionner la balise, déterminer l'index de la balise 
             let deleteItems = document.querySelectorAll(".deleteItem");
-
-
-            // handle quantity changes  
-            handleChange(qtyInputs, contentInCart);
-            // delete a product
-            handleDelete(deleteItems, contentInCart);
+            deleteItems.forEach((deleteItem, index) =>{
+                let indexToDelete = index;
+                console.log(index);
+                deleteItem.addEventListener("click", (e) => {
+                    console.log(e.target, index);
+                    
+                    // récupérer les infos du produit concerné par le changement d'input (son id et sa couleur) et tout sauvegarder dans des variables
+                    let id = e.currentTarget.closest(".cart__item").dataset.id;
+                    console.log(e.currentTarget.closest(".cart__item"));
+                    let color = e.currentTarget.closest(".cart__item").dataset.color;
+                    console.log(color);
+                    console.log(contentInCart);
+                    const newContentInCart = contentInCart.filter((productObj)=>{ return !(id === productObj.id && color === productObj.color)})
+                    console.log(newContentInCart);
+                    // écraser l'ancien localStorage avec la nouvelle valeur (result) et recharger la page
+                    localStorage.setItem("Cart", JSON.stringify(newContentInCart));
+                    cartItems.innerHTML = "";
+                    location.reload();
+                }) // fin de EventListener sur deleteItem ligne 182
+            })//fin de forEach sur deleteItems ligne 179
 
         // PARTIE FORMULAIRE
         // 1. sélectionner les balises à remplir et les 5 balises pour les messages d'erreur
